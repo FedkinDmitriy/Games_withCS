5fc3412901086d22ffa272883fc06f900edf02ed
FedkinDmitriy <fedkindmitriy@yandex.ru>
2024-08-29 17:13:06 +0300

    Добавьте файлы проекта.

---
+++ GuessNumber_CS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GuessNumber_CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Randomizer randomizer = new Randomizer(0,1000);
+            randomizer.RegisterDelegats(UserInput,StringHandler);
+            randomizer.LetsPlay();
+        }
+
+        static int UserInput()
+        {
+            int.TryParse(Console.ReadLine(), out int num);
+            return num;
+        }
+        static void StringHandler(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ GuessNumber_CS/Randomizer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace GuessNumber_CS
+{
+    public delegate int UserInput();
+    public delegate void GetStatus(string message);
+    public class Randomizer
+    {
+        private UserInput _usr;
+        private GetStatus _sts;
+        private int _from, _to;
+        private int _guessNumber;
+        private bool isGuess = true;
+
+        public Randomizer(int from = 0, int to = 100)
+        {
+            _from = from;
+            _to = to;
+            _guessNumber = new Random().Next(_from, _to);
+        }
+        public bool RegisterDelegats(UserInput del1, GetStatus del2)
+        {
+            _usr = del1;
+            _sts = del2;
+            if (_usr != null && _sts != null)
+            {
+                return true;
+            }
+            else return false;
+        }
+        public void LetsPlay()
+        {
+            while (isGuess)
+            {
+                int? userInput = _usr?.Invoke();
+
+                if (userInput != _guessNumber)
+                {
+                    if (userInput < _guessNumber)
+                    {
+                        _sts?.Invoke($"Твоё число меньше загаданного, попробуй ещё");
+                    }
+                    else if (userInput > _guessNumber)
+                    {
+                        _sts?.Invoke($"Твоё число больше загаданного, попробуй ещё");
+                    }
+                }
+                else
+                {
+                    _sts?.Invoke($"Ты угадал, поздравляю !!!");
+                    isGuess = false;
+                }
+            }
+
+        }
+
+    }
+}
